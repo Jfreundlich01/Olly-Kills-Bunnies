dc9b29bba2ad8a1be7cc8cc532f5882691f39b0e
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-21 13:58:19 -0400

    added coyote that hunts you

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let ctx = game.getContext("2d");
 game.setAttribute("height", getComputedStyle(game)["height"]);
 game.setAttribute("width", getComputedStyle(game)["width"]);
 let oly;
+let bunny1
+let bunny2
+let bunny3
+let coyote
 
 class Character {
   constructor(x, y, color, width, height, speed) {
@@ -49,13 +53,17 @@ class Character {
 }
 
 class Oly extends Character {
-  constructor(x, y, img, width, height, speed) {
-    super(x, y, img, width, height, speed);
+  constructor(x, y, color, width, height, speed) {
+    super(x, y, color, width, height, speed);
     this.bite = false;
     this.hasBunny = false;
   }
 
   //Oly's move mechanics. Follows arrow keystrokes
+    // sayHi(){
+    //     return "hi"
+    // }
+
     // move(e) {
     //   console.log(`the movement was ${e.key}`);
 
@@ -91,18 +99,18 @@ class Oly extends Character {
 
 }
 
-// class Bunny extends Character {
-//     constructor(x, y, img, width, height, speed) {
-//         super(x, y, img, width, height, speed);
-//     this.held = false;
-//     }
-//     //Add score once dead bunny is dropped on stoop. 
-//     scoreCount(bunny){
-//         if(!bunny.alive && x === 73 && y === 76){
-//             addScore();
-//         }
-//     }
-// }
+class Bunny extends Character {
+    constructor(x, y, img, width, height, speed) {
+        super(x, y, img, width, height, speed);
+    this.held = false;
+    }
+    //Add score once dead bunny is dropped on stoop. 
+    scoreCount(bunny){
+        if(!bunny.alive && x === 73 && y === 76){
+            addScore();
+        }
+    }
+}
 
 // class David extends Character {
 //     constructor(x, y, img, width, height, speed) {
@@ -121,28 +129,39 @@ class Oly extends Character {
 
 window.addEventListener("DOMContentLoaded", function(){
     (function (){
-       oly = new Oly(900,620,'Grey',40,40,3);
-       //const floor = document.querySelector(".floor")
-       //floor.style.backgroundImage = "url('../img/betterGrass.png')"; // Oly was rendering under the floor. Tried to get it so he would redner after floor img loaded. Didn't work. Will revist later. 
-       bunny1 = new Character(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"white",20,20,.5)
-       bunny2 = new Character(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"white",20,20,.5)
-       bunny3 = new Character(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"white",20,20,.5)
+        //const floor = document.querySelector(".floor")
+        //floor.style.backgroundImage = "url('../img/betterGrass.png')"; // Oly was rendering under the floor. Tried to get it so he would redner after floor img loaded. Didn't work. Will revist later. 
+        
+        //create my game characters
+        oly = new Oly(900,620,'Grey',40,40,3);
+        bunny1 = new Bunny(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"white",20,20,.5)
+        bunny2 = new Bunny(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"white",20,20,.5)
+        bunny3 = new Bunny(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"white",20,20,.5)
+        coyote = new Character(Math.floor(Math.random() * 900),Math.floor(Math.random() * 600),"brown", 55,55,.7)
         // floor.onload = function(){
-        // }
-        oly.render();
-        bunny1.render();
-        bunny2.render();
-        bunny3.render();
-        console.log(oly)
-        console.log(bunny1)
-
-        const runGame = setInterval(gameLoop, 120)
-    })()
-})
-
+            // }
+            
+            //Render my Game Characters
+            oly.render();
+           bunny1.render();
+           bunny2.render();
+           bunny3.render();
+           coyote.render();
+           console.log(oly,1)
+           console.log(bunny1,2)
+           
+           //run the game
+           const runGame = setInterval(gameLoop, 120)
+        })()
+        document.addEventListener("keydown", movementHandler)
+        document.addEventListener("keyup", moveHandler2)
+         
+    })
+    
+    //Oly Move Function Key Down
 function movementHandler(e){
-    //console.log(`the movement was ${e.key}`)
-    console.log(`OlyBite is ${oly.bite}`)
+    //console.log(`the movement was ${e.key}`, 3)
+    console.log(`OlyBite is ${oly.bite}`,4)
     switch (e.key){
         case "ArrowUp": 
         oly.y >=0 ? oly.y -= 5 * oly.speed : null
@@ -165,29 +184,47 @@ function movementHandler(e){
     //console.log(oly)
 }
 
-function movehandler2(e){
-    console.log(`OlyBite is ${oly.bite}`)
+//Oly Seconday Move Function Key Up
+function moveHandler2(e){
+    console.log(`OlyBite is ${oly.bite}`,5)
     switch (e.key){
         case " " :
             oly.bite = false;
     }
 }
 
-document.addEventListener("keydown", movementHandler)
-document.addEventListener("keyup", movehandler2)
+function coyoteHunt(){
+    if(coyote.x < oly.x){
+        coyote.x += 5 * coyote.speed
+    } else {
+        coyote.x -= 5 * coyote.speed
+    }
 
+    if(coyote.y < oly.y){
+        coyote.y += 5 * coyote.speed
+    }  else {
+        coyote.y -= 5 * coyote.speed
+    } 
+}
+//listen to key up and down
+
+//Game Loop
 function gameLoop(){
+    //clear baord every loop
     ctx.clearRect(0,0, game.width, game.height)
+    //render my characters every loop
     oly.render();
     bunny1.render();
     bunny2.render();
     bunny3.render();
+    coyote.render();
     detectHit(oly,bunny1)
     detectHit(oly,bunny2)
     detectHit(oly,bunny3)
+    coyoteHunt();
     
 }
-
+//detect hit function
 function detectHit(p1,p2){
     let hitTest =
     p1.y + p1.height > p2.y &&
@@ -196,10 +233,12 @@ function detectHit(p1,p2){
     p1.x < p2.x + p2.width; // {boolean} : if all are true -> hit 
 
     if(hitTest){
-        console.log("Hit")
+        console.log("Hit",6)
+        //check for bite
         if(oly.bite){
-            console.log("Hit and Bit")
+            console.log("Hit and Bit",7)
             p2.color = "red"
+            //attach bunnies to Oly
             p2.x = oly.x
             p2.y = oly.y
         }
