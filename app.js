1c1f71a479a7d0233d9d1d98b0ee1b1adfadf0af
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-21 11:12:40 -0400

    Oly wiring. Movehandlers, outside class. Bite keydown and keyup

--- app.js
+++ app.js
@@ -2,40 +2,50 @@ const game = document.querySelector("#game");
 let ctx = game.getContext("2d");
 game.setAttribute("height", getComputedStyle(game)["height"]);
 game.setAttribute("width", getComputedStyle(game)["width"]);
+let oly;
 
 class Character {
-  constructor(x, y, img, width, height, speed) {
+  constructor(x, y, color, width, height, speed) {
     this.x = x;
     this.y = y;
-    this.img = img;
+    this.color = color;
     this.width = width;
     this.height = height;
     this.speed = speed;
     this.alive = true;
     this.hunting = false;
   }
+
   //renders images onto canvas
-  drawImage(image, dx, dy, dWidth, dHeight) {}
+  //image, destination x, destination y, destination width, destination height
+//   drawImage() {
+//       ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+//   }
 
-  //very basic move function for non Oly Characters
-  move() {
-    x += Math.floor(Math.random() * game.width);
-    y += Math.floor(Math.random() * game.height);
-  }
-
-  //The hunt mechanic for Coyote, and David. 
-  hunt(oly,predator){
-    predator.x = oly.x;
-    predator.y = oly.y;
-  }
-  
-  //die/hitTest
-  die(p1,p2){
-    if(hitTest){
-
-    }
+  render(){
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x,this.y,this.width, this.height)
 }
+
+
+//   //very basic move function for non Oly Characters
+//   move() {
+//     x += Math.floor(Math.random() * game.width);
+//     y += Math.floor(Math.random() * game.height);
+//   }
+
+//   //The hunt mechanic for Coyote, and David. 
+//   hunt(oly,predator){
+//     predator.x = oly.x;
+//     predator.y = oly.y;
+//   }
   
+//   //die/hitTest
+//   die(p1,p2){
+//     if(hitTest){
+
+//     }
+// }
 }
 
 class Oly extends Character {
@@ -46,69 +56,124 @@ class Oly extends Character {
   }
 
   //Oly's move mechanics. Follows arrow keystrokes
-  move() {
-    function movementHandler(e) {
-      console.log(`the movement was ${e.key}`);
+    // move(e) {
+    //   console.log(`the movement was ${e.key}`);
 
-      switch (e.key) {
-        case "ArrowUp":
-          oly.y >= 0 ? (oly.y -= 5) : null;
-          break;
+    //   switch (e.key){
+    //     case "ArrowUp": 
+    //     oly.y >=0 ? oly.y -= 5 * oly.speed : null
+    //         break
+        
+    //     case "ArrowDown":
+    //     oly.y += 5 * oly.speed;
+    //         break
 
+    //     case "ArrowLeft":
+    //         oly.x -= 5 * oly.speed;
+    //         break
+
+    //     case "ArrowRight":
+    //         oly.x += 5 * oly.speed;
+    //         break
+    
+    // }
+    //   console.log(oly);
+    // }
+  //Oly bite mechanic. Space bar click bites. If collision with deadBunny and bite ==> hasBunny. 
+//   bite(e){
+//     if(e.key === " "){
+//         console.log(`it is ${this.bite} about oly's bite`)
+//         return this.bite = true;
+//         // killBunny(oly.aliveBunny)
+//         // append(oly.deadBunny)  
+//     }
+//   }
+
+}
+
+// class Bunny extends Character {
+//     constructor(x, y, img, width, height, speed) {
+//         super(x, y, img, width, height, speed);
+//     this.held = false;
+//     }
+//     //Add score once dead bunny is dropped on stoop. 
+//     scoreCount(bunny){
+//         if(!bunny.alive && x === 73 && y === 76){
+//             addScore();
+//         }
+//     }
+// }
+
+// class David extends Character {
+//     constructor(x, y, img, width, height, speed) {
+//         super(x, y, img, width, height, speed);
+// }
+//     //Activates David hunt. Switches his img to Angry David. 
+//     getAngry(){
+//         if(hunting){
+//             img.src = "Angry David"
+//             this.hunt(oly,david)
+//         } else {
+//             img.src = "nice David"
+//         }
+//     }
+// }
+
+window.addEventListener("DOMContentLoaded", function(){
+    (function (){
+       oly = new Oly(900,620,'Grey',40,40,3);
+       //const floor = document.querySelector(".floor")
+       //floor.style.backgroundImage = "url('../img/betterGrass.png')"; // Oly was rendering under the floor. Tried to get it so he would redner after floor img loaded. Didn't work. Will revist later. 
+       bunny1 = new Character(100,100,"white",20,20,.5)
+        // floor.onload = function(){
+        // }
+        oly.render();
+        bunny1.render();
+        console.log(oly)
+        console.log(bunny1)
+
+        const runGame = setInterval(gameLoop, 120)
+    })()
+})
+
+function movementHandler(e){
+    console.log(`the movement was ${e.key}`)
+    console.log(`OlyBite is ${oly.bite}`)
+    switch (e.key){
+        case "ArrowUp": 
+        oly.y >=0 ? oly.y -= 5 * oly.speed : null
+            break
+        
         case "ArrowDown":
-          oly.y += 5;
-          break;
+        oly.y += 5 * oly.speed;
+            break
 
         case "ArrowLeft":
-          oly.x -= 5;
-          break;
+            oly.x -= 5 * oly.speed;
+            break
 
         case "ArrowRight":
-          oly.x += 5;
-          break;
-      }
-      console.log(donkey);
-    }
-  }
-
-  //Oly bite mechanic. Space bar click bites. If collision with deadBunny and bite ==> hasBunny. 
-  bite(e){
-    switch(e.key){
+            oly.x += 5 * oly.speed;
+            break
         case " ":
-        this.bite = true;
-        killBunny(oly.aliveBunny)
-        append(oly.deadBunny)  
+            oly.bite = true;
     }
-  }
-
+    console.log(oly)
 }
 
-class Bunny extends Character {
-    constructor(x, y, img, width, height, speed) {
-        super(x, y, img, width, height, speed);
-    this.held = false;
-    }
-    //Add score once dead bunny is dropped on stoop. 
-    scoreCount(bunny){
-        if(!bunny.alive && x === 73 && y === 76){
-            addScore();
-        }
+function movehandler2(e){
+    console.log(`OlyBite is ${oly.bite}`)
+    switch (e.key){
+        case " " :
+            oly.bite = false;
     }
 }
 
-class David extends Character {
-    constructor(x, y, img, width, height, speed) {
-        super(x, y, img, width, height, speed);
-}
-    //Activates David hunt. Switches his img to Angry David. 
-    getAngry(){
-        if(hunting){
-            img.src = "Angry David"
-            this.hunt(oly,david)
-        } else {
-            img.src = "nice David"
-        }
-    }
-}
+document.addEventListener("keydown", movementHandler)
+document.addEventListener("keyup", movehandler2)
 
-
+function gameLoop(){
+    ctx.clearRect(0,0, game.width, game.height)
+    oly.render();
+    bunny1.render();
+}
